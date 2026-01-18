04dc8cc6ec843c81c6e056228f8fce11ba36874c
ShivanshTripathi247 <shivansht06@gmail.com>
2026-01-18 15:49:26 +0530

    email service redone

--- backend/src/adapters/emailAdapter.js
+++ backend/src/adapters/emailAdapter.js
@@ -12,7 +12,7 @@ class emailSender {
         this.transporter = nodemailer.createTransport({
             host: SMTP_HOST,
             port: SMTP_PORT,
-            secure: false,
+            secure: true,
             auth: {
                 user: SMTP_USER,
                 pass: SMTP_PASS,
